59fd52e98569c07f704e3713b7e217c88be02ab5
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-03-14 15:32:37 +0530

    Dockerized simple spring boot app and pushing it to github

---
+++ src/test/java/com/mindtree/simplespringbootdocker/SimpleSpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.simplespringbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
